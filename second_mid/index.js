9265824f9206a011c140033138b686af960f2c06
Haseeb Ullah <53255366+HaseebUllahAbbasi@users.noreply.github.com>
2022-04-23 09:46:59 +0500

    ejs basic practice

--- second_mid/index.js
+++ second_mid/index.js
@@ -2,19 +2,50 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/home.html"));
+// defined the view engine as ejs 
+app.use('view engine','ejs');
 
-  //   res.status(200).json({
-  //     success: true,
-  //     message: "Home Page",
-  //   });
+/**
+ *  
+  the styling will be the public url for the web page .
+ *  
+ * app.use('/styling', express.static('public/style'))
+ *
+ 
+
+  static will be the public url source for the webpage
+  
+  *
+  * app.use('/static', express.static('public'))
+  *   
+  
+  another usage 
+  *
+  * app.use('/static', express.static(path.join(__dirname, 'public')))  
+  * 
+  
+ 
+**/
+
+app.get("/", (req, res) => {
+
+  const products = ['O','P', 'Q']; 
+  res.render('home', {name: "Dot", products})
+
+  // res.sendFile(path.join(__dirname, "views/home.html"));
+
+    // res.status(200).json({
+    //   success: true,
+    //   message: "Home Page",
+    // });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "views/about.html"));
+  res.render('about')
+  // res.sendFile(path.join(__dirname, "views/about.html"));
 });
 
 app.listen(4000, () => {
