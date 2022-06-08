59415af419fa6b649bf0ec40ab6c64e1d21b3179
Haseeb U <haseebabbasi00@gmail.com>
2022-06-08 13:19:07 +0500

    code refactored

--- Basic-Crud-APP-Using-EJS-master/controller/UserController.js
+++ Basic-Crud-APP-Using-EJS-master/controller/UserController.js
@@ -2,64 +2,64 @@ const UserSchema = require("../models/UserModel");
 const bcrypt = require("bcrypt");
 
 async function authenticate(req) {
-    const { email, password } = req.body;
-    let errmsg = ""
-    const user = await UserSchema.findOne({ email: email.trim() });
-    if (user) {
-        const match = await bcrypt.compare(password, user.password);
-        if (match) {
-            req.session.uid = user._id;
-            isCurrentlySignIn = true;
-            return true;
-        }
-    } else {
-        console.log("Please enter a valid name or user name")
-        errmsg = "Please enter a valid name or user name";
-        return false;
+  const { email, password } = req.body;
+  let errmsg = "";
+  const user = await UserSchema.findOne({ email: email.trim() });
+  if (user) {
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
+      req.session.uid = user._id;
+      isCurrentlySignIn = true;
+      return true;
     }
-
+  } else {
+    console.log("Please enter a valid name or user name");
+    errmsg = "Please enter a valid name or user name";
+    return false;
+  }
 }
 exports.Login = async (req, res) => {
-    let isfound = await authenticate(req);
-    if (isfound) {
-        req.session.isAuth = true;
-        res.redirect("/")
-    }
-    else
-        res.redirect("signin")
-}
+  let isfound = await authenticate(req);
+  if (isfound) {
+    req.session.isAuth = true;
+    res.redirect("/");
+  } else res.redirect("signin");
+};
 
 exports.Logout = async (req, res) => {
-    req.session.destroy();
-    res.redirect("/signin");
-}
+  req.session.destroy();
+  res.redirect("/signin");
+};
 exports.Register = async (req, res) => {
-    const { name, email, password } = req.body
-    console.log(password)
-    const findUser = UserSchema.findOne({ email })
-    if (!findUser) {
-        const result = await UserSchema.create({ name: name.trim(), email: email.trim(), password: password });
-        console.log(result)
-        if (result) {
-            res.redirect("signin")
-        } else {
-            console.log("Please enter a valid name or user name")
-            errmsg = "Please enter a valid name or user name";
-            res.render("signup")
-        }
+  const { name, email, password } = req.body;
+  console.log(password);
+  const findUser = await UserSchema.find({ email });
+  if (findUser.length == 0) {
+    const result = await UserSchema.create({
+      name: name.trim(),
+      email: email.trim(),
+      password: password,
+    });
+    console.log(result);
+    if (result) {
+      res.redirect("signin");
+    } else {
+      console.log("Please enter a valid name or user name");
+      errmsg = "Please enter a valid name or user name";
+      res.render("signup");
     }
-    else {
-        console.log("email already registered")
-        errmsg = "Please enter an unused email";
-        res.render("signup")
-    }
-}
+  } else {
+    console.log("email already registered");
+    errmsg = "Please enter an unused email";
+    res.render("signup");
+  }
+};
 exports.RegisterPage = async (req, res) => {
-    res.render("signup")
-}
+  res.render("signup");
+};
 exports.LoginPage = async (req, res) => {
-    res.render("signin")
-}
+  res.render("signin");
+};
 
 // exports.signInPage = (req, res) => {
 //     let method = req.params.method;
--- Basic-Crud-APP-Using-EJS-master/models/UserModel.js
+++ Basic-Crud-APP-Using-EJS-master/models/UserModel.js
@@ -1,24 +1,25 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const UserSchema = mongoose.Schema({
-    name: {
-        type: String
-    },
-    email:{
-        type: String,
-        required:[true, "Please Enter Email"]
-    },
-    password:{
-        type: String,
-        required:[true, "Please Enter Password"]
-    }
-})
-UserSchema.pre("save", function(next){
-    const user = this;
-    bcrypt.hash(user.password,5, function(req,hash){
-        user.password = hash;
-        next();
-    })
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: [true, "Please Enter Email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please Enter Password"],
+  },
+});
+UserSchema.pre("save", function (next) {
+  const user = this;
+  bcrypt.hash(user.password, 5, function (req, hash) {
+    user.password = hash;
+    next();
+  });
 });
 
-module.exports = mongoose.model("users",UserSchema)
+module.exports = mongoose.model("users", UserSchema);
